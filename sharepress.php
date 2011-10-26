518a0eb404a54ec926f1c7cedca3860a99f67bda
Aaron Collegeman <aaron@collegeman.net>
2011-10-26 14:44:24 -0400

    2.0.20 - forget to bump the version

--- sharepress.php
+++ sharepress.php
@@ -5,7 +5,7 @@ Plugin URI: http://aaroncollegeman.com/sharepress
 Description: SharePress publishes your content to your personal Facebook Wall and the Walls of Pages you choose.
 Author: Fat Panda, LLC
 Author URI: http://fatpandadev.com
-Version: 2.0.19
+Version: 2.0.20
 License: GPL2
 */
 
