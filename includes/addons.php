a8cf5c51f09c41a6f6dbd968f9802d93f5163a84
Joey Blake <joeyblake@gmail.com>
2013-12-29 22:54:21 -0500

    added addons css, fontawesome icons etc

--- includes/addons.php
+++ includes/addons.php
@@ -18,7 +18,154 @@ function sp_addons_menu() {
   }
 }
 
+function sp_is_plugin_active() {
+  return false;
+}
+
 function sp_addons_page() {
+  $premium = array();
+  $free = array();
+
+  $premium[] = array(
+    'title' => 'Support',
+    'description' => 'Technical support from SharePress developers, and advanced access to new features: $25/year',
+    'icon' => 'fa-wrench',
+    'icon-color' => '',
+    'bg' => '#FFA000',
+    'active' => sp_is_plugin_active('support')
+  );
+
+  $premium[] = array(
+    'title' => 'Twitter',
+    'description' => 'customize your Tweets: $19/year',
+    'icon' => 'fa-twitter',
+    'icon-color' => '',
+    'bg' => '#6EB4F9',
+    'active' => sp_is_plugin_active('sharepress-twitter')
+  );
+
+  $premium[] = array(
+    'title' => 'Facebook Pages',
+    'description' => 'post to Facebook Pages: $19/year',
+    'icon' => 'fa-facebook',
+    'icon-color' => '',
+    'bg' => '#3B5B93',
+    'active' => sp_is_plugin_active('sharepress-facebook-pages')
+  );
+
+  $premium[] = array(
+    'title' => 'Google+',
+    'description' => 'post to Google+ Profiles: $9/year',
+    'icon' => 'fa-google-plus',
+    'icon-color' => '',
+    'bg' => '#E15B52',
+    'active' => sp_is_plugin_active('sharepress-googleplus')
+  );
+
+  $premium[] = array(
+    'title' => 'LinkedIn',
+    'description' => 'post to LinkedIn Profiles: $9/year',
+    'icon' => 'fa-linkedin',
+    'icon-color' => '',
+    'bg' => '#57A9CB',
+    'active' => sp_is_plugin_active('sharepress-linkedin')
+  );
+
+  $premium[] = array(
+    'title' => 'Repeater',
+    'description' => 'automatically repeat posts: $25/year',
+    'icon' => 'fa-refresh',
+    'icon-color' => '',
+    'bg' => '#FFC46A',
+    'active' => sp_is_plugin_active('sharepress-repeater')
+  );
+
+  $premium[] = array(
+    'title' => 'Buffer',
+    'description' => 'stop scheduling and start buffering posts: feed the beast!: $25/year',
+    'icon' => 'fa-th-list',
+    'icon-color' => '',
+    'bg' => '#18944B',
+    'active' => sp_is_plugin_active('sharepress-buffer')
+  );
+
+  $premium[] = array(
+    'title' => 'Authors',
+    'description' => 'post to your Authors\' social media profiles, too: $25/year',
+    'icon' => 'fa-pencil',
+    'icon-color' => '',
+    'bg' => '#819197',
+    'active' => sp_is_plugin_active('sharepress-authors')
+  );
+
+  $premium[] = array(
+    'title' => 'Advanced Social Metadata',
+    'description' => 'Twitter Cards, and custom meta data on a service-by-service basis: $25/year',
+    'icon' => 'fa-bullhorn',
+    'icon-color' => '',
+    'bg' => '#2675B2',
+    'active' => sp_is_plugin_active('sharepress-advanced-metadata')
+  );
+
+  $premium[] = array(
+    'title' => 'Calendar',
+    'description' => 'see what you have scheduled, and manage your editorial cycle: $25/year',
+    'icon' => 'fa-calendar',
+    'icon-color' => '',
+    'bg' => '#945A56',
+    'active' => sp_is_plugin_active('sharepress-calendar')
+  );
+
+  $premium[] = array(
+    'title' => 'Meme',
+    'description' => 'add text to any image in your library: $15/year',
+    'icon' => 'fa-fighter-jet',
+    'icon-color' => '',
+    'bg' => '#50B2FE',
+    'active' => sp_is_plugin_active('sharepress-meme')
+  );
+
+  $premium[] = array(
+    'title' => 'Convo',
+    'description' => 'bring the conversation home: comments and forums, mobile-ready: $99/year',
+    'icon' => 'fa-beer',
+    'icon-color' => '',
+    'bg' => '#4A3E31',
+    'active' => sp_is_plugin_active('sharepress-convo')
+  );
+
+  $premium[] = array(
+    'title' => 'iPhone App',
+    'description' => 'wrap your responsive WordPress site for the App Store, announce new content with push notifications: starts at $299/year',
+    'icon' => 'fa-apple',
+    'icon-color' => '',
+    'bg' => '#FE9343',
+    'active' => sp_is_plugin_active('sharepress-iphone-app')
+  );
+
+  $premium[] = array(
+    'title' => 'Android App',
+    'description' => 'wrap your responsive WordPress site for Google Play: starts at $299/year',
+    'icon' => 'fa-android',
+    'icon-color' => '',
+    'bg' => '#4CB238',
+    'active' => sp_is_plugin_active('sharepress-android-app')
+  );
+
+  $free[] = array(
+    'title' => 'Social Login',
+    'description' => 'allow your users to login for commenting: free',
+    'icon' => '',
+    'icon-color' => '',
+    'bg' => '',
+    'active' => sp_is_plugin_active('sharepress-social-login')
+  );
+
   wp_enqueue_style('sp-addons', SP_URL.'/css/addons.css');
-  sp_require_view('addons');
+  wp_enqueue_style('sp-fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
+  sp_require_view('addons', array(
+    'premium' => $premium,
+    'free' => $free
+    )
+  );
 }
--- views/addons.php
+++ views/addons.php
@@ -2,7 +2,56 @@
 
   <div class="icon32" id="icon-acf"><br></div>
   <h2 style="margin: 4px 0 15px;">SharePress Add-Ons</h2>
+
+  <div class="acf-alert">
+    <p style="">The following Add-ons are available to increase the functionality of the SharePress plugin.<br>Each Add-on can be installed as a separate plugin (receives updates).</p>
+  </div>
   
-  
+  <div id="add-ons" class="clearfix">
+    <div class="add-on-group clearfix">
+      <?php foreach( $premium as $add_on ) { ?>
+        <div class="add-on wp-box add-on-active" >
+          <a target="_blank" href="<?php echo $add_on['url'] ?>" style="display:block; padding:15px; text-align:center; font-size:10em; background-color:<?php echo $add_on['bg'] ?>; color:#fff"><i class="fa <?php echo $add_on['icon'] ?>"></i></a>
+          <div class="inner">
+            <h3><a target="_blank" href="<?php echo $add_on['url'] ?>"><?php echo $add_on['title'] ?></a></h3>
+            <p><?php echo $add_on['description'] ?></p>
+          </div>
+          <div class="footer">
+            <div style="padding: 10px;">
+              <?php if ( $add_on['active'] ) { ?>
+                <a class="button button-disabled"><span class="acf-sprite-tick"></span>Installed</a>
+              <?php } else { ?>
+                <a target="_blank" href="<?php echo $add_on['url'] ?>" class="button"><span class="acf-sprite-tick"></span>Purchase &amp; Install</a>
+              <?php } ?>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
+    
+    <div class="add-on-group clearfix">
+        
+    </div>
+      
+  </div>
   
 </div>
+
+<script type="text/javascript">
+(function($) {
+  
+  $(window).load(function(){
+    
+    $('#add-ons .add-on-group').each(function(){
+      var $el = $(this),
+        h = 0;
+      $el.find('.add-on').each(function(){
+        h = Math.max( $(this).height(), h );
+      });
+      $el.find('.add-on').height( h );
+    });
+    
+  });
+  
+})(jQuery); 
+</script>
